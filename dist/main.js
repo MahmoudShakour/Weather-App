d121519873bf1f2fd47cb67c6a689837048e51cc
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-03-25 17:11:32 +0200

    initialize project

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{console.log("hi");const e=document.querySelector(".root"),t=document.createElement("div");t.textContent="hi",e.append(t)})();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('hi');
+
+const root=document.querySelector('.root');
+
+const div=document.createElement('div');
+div.textContent="hi";
+
+root.append(div);
